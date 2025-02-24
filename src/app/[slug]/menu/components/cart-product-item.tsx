9b7f00d5beea4112217d4c060b44d9ad59d54898
Priscila Maria <primvjc@hotmail.com>
2025-02-24 18:57:09 -0300

    feat: ensure cart product quantity can be decreased

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,16 +1,18 @@
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 
-import { CartProduct } from "../contexts/cart";
+import { CartContext, CartProduct } from "../contexts/cart";
 
 interface CartProductItemProps {
     product: CartProduct
 }
 
 const CartProductItem = ({product}: CartProductItemProps) => {
+    const {decreaseProductQuantity} = useContext(CartContext)
     return ( 
     <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
@@ -21,7 +23,7 @@ const CartProductItem = ({product}: CartProductItemProps) => {
             <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
             <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
             <div className="flex items-center gap-1 text-center">
-                <Button className="h-7 w-7 rounded-lg" variant="outline">
+                <Button className="h-7 w-7 rounded-lg" variant="outline" onClick={() => decreaseProductQuantity(product.id)}>
                     <ChevronLeftIcon />
                 </Button>
                 <p className="w-7 text-xs">{product.quantity}</p>
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -13,13 +13,15 @@ export interface ICartContext {
     products: CartProduct[];
     toggleCart: () => void;
     addProduct: (product: CartProduct) => void;
+    decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
     isOpen: false,
     products: [],
     toggleCart: () => {},
-    addProduct: () => {}
+    addProduct: () => {},
+    decreaseProductQuantity: () => {}
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -48,13 +50,27 @@ const addProduct = (product: CartProduct) => {
       });
     });
   };
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts(prevProducts => {
+      return prevProducts.map(prevProduct => {
+        if (prevProduct.id !== productId) {
+          return prevProduct
+        }
+        if (prevProduct.quantity == 1) {
+          return prevProduct;
+        }
+        return {...prevProduct, quantity: prevProduct.quantity -1}
+      });
+    });
+  };
   return (
     <CartContext.Provider
       value={{
         isOpen,
         products,
         toggleCart,
-        addProduct
+        addProduct,
+        decreaseProductQuantity
       }}
     >
       {children}
